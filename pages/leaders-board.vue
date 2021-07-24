27213c742b876bc0fc55c70494a7ed7df0506f45
geekmaros <razicruz@gmail.com>
2021-07-24 23:38:41 +0100

    1. added leader board ranking 2. added user avatar on ranking board 3. added total points

--- pages/leaders-board.vue
+++ pages/leaders-board.vue
@@ -8,11 +8,41 @@
       <div class="headline accent--text font-weight-medium">Leader board</div>
       <div class="py-2">
         <span>Total points earned:</span>
-        <strong>00</strong>
+        <strong>{{ totalPoints }}</strong>
       </div>
     </header>
 
-    <program-hall-of-fame />
+    <section class="elevation-2">
+      <!-- <div>You have no group yet</div> -->
+      <v-simple-table>
+        <template #default>
+          <thead class="font-weight-medium secondary accent--text">
+            <td class="pa-4">Rank</td>
+            <td class="px-6 py-4">Researcher</td>
+            <td class="pa-4">Points</td>
+          </thead>
+          <tbody>
+            <tr v-for="(leader, index) in leaderBoard" :key="`leader-${index}`">
+              <td>{{ index + 1 }}</td>
+              <td>
+                <v-avatar size="35" class="mr-4" v-if="leader.profile[0].image">
+                  <img :src="leader.profile[0].image" alt="" />
+                </v-avatar>
+                <v-avatar size="35" class="mr-4" v-else>
+                  <v-icon color="accent">mdi-account-circle</v-icon></v-avatar
+                >{{ leader.profile[0].username }}
+                <v-avatar size="30">
+                  <img v-if="index === 0" src="/img/gold-medal.png" alt="" />
+                  <img v-if="index === 1" src="/img/silver-medal.png" alt="" />
+                  <img v-if="index === 2" src="/img/bronze-medal.png" alt="" />
+                </v-avatar>
+              </td>
+              <td>{{ leader.points }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </section>
   </main>
 </template>
 
@@ -28,15 +58,24 @@ export default {
         },
         { disabled: true, text: 'Leader board' },
       ],
+      leaderBoard: [],
+      totalPoints: 0,
     }
   },
   async fetch() {
-    const URL = `/get-leaders-board/?page={1}&limit={10}`
+    const URL = `/get-leaders-board`
     // Make upload request to the API
     await this.$axios
       .$get(URL)
       .then((res) => {
-        console.log(res)
+        console.log(res.data)
+        const sum = res.data.docs
+        this.leaderBoard = res.data.docs
+        this.totalPoints = sum.reduce(function (a, b) {
+          return a + b.points
+        }, 0)
+
+        console.log(this.totalPoints)
         // this.pagination.length = res.data.totalPages
       })
       .catch((error) => {
@@ -49,5 +88,7 @@ export default {
         })
       })
   },
+  mounted() {},
+  methods: {},
 }
 </script>
