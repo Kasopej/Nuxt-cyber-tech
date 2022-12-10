d8e479c99bfd924d3a131cf5399d4c4e3ccc486d
Kasope Johnson <kasopej@gmail.com>
2022-12-10 09:16:13 +0100

    displays number of programs & submissions received from API

--- pages/index.vue
+++ pages/index.vue
@@ -1,98 +1,110 @@
 <template>
-  <!-- <v-row class="py-4">
-    <v-col cols="12" md="6">
-      <v-card class="px- pb-8">
-        <v-tabs grow>
-          <v-tab>
-            <v-icon class="mr-3">mdi-gift-outline</v-icon>
-            <span class="text-capitalize">Rewards</span>
-          </v-tab>
-          <v-tab>
-            <v-icon class="mr-3">mdi-comment-text-multiple-outline</v-icon>
-            <span class="text-capitalize">Messages</span>
-          </v-tab>
-
-          <v-tab-item>
-            <dashboard-rewards />
-          </v-tab-item>
-
-          <v-tab-item>
-            <dashboard-messages />
-          </v-tab-item>
-        </v-tabs>
-      </v-card>
-    </v-col>
-
-    <v-col cols="12" md="6">
-      <dashboard-announcement v-if="notificationState" />
-    </v-col>
-  </v-row> -->
   <v-main>
-    <v-card raised hover class="mb-6 no-cursor">
-      <v-card-title primary-title>
-        <v-icon color="primary" x-large left>mdi-shield-account</v-icon>
-        Your Programs and Submissions</v-card-title
-      >
-      <v-card-text class="d-flex">
-        <v-col cols="6" md="4" class="d-flex align-start">
-          <v-icon color="accent" left>mdi-shield-lock</v-icon>
-          <div class="cursor" @click="() => $router.push('/program')">
-            <h3 class="w-max">Private Programs</h3>
-            <span class="max-content mx-auto text-h3">{{ 10 }}</span>
-          </div>
-        </v-col>
-        <v-col cols="6" md="4" class="d-flex align-start">
-          <v-icon color="accent" left>mdi-shield-link-variant</v-icon>
-          <div class="cursor" @click="() => $router.push('/submission')">
-            <h3 class="w-max">Submissions</h3>
-            <span class="fit-content mx-auto text-h3">{{ 10 }}</span>
-          </div>
-        </v-col>
-      </v-card-text>
-    </v-card>
-    <v-card raised hover class="mb-6 no-cursor">
-      <v-card-title primary-title>
-        <v-icon color="primary" x-large left>mdi-podium-gold</v-icon>
-        Your Ranking</v-card-title
-      >
-      <v-card-text class="d-flex">
-        <v-col cols="6" md="4" class="d-flex align-start">
-          <v-icon color="accent" left>mdi-star-box</v-icon>
-          <div class="cursor">
-            <h3 class="w-max">Points Earned</h3>
-            <span class="fit-content mx-auto text-h3">{{ 10 }}</span>
-          </div>
-        </v-col>
-        <v-col cols="6" md="4" class="d-flex align-start">
-          <v-icon color="accent" left>mdi-seal</v-icon>
-          <div class="cursor">
-            <h3 class="w-max">Badges</h3>
-            <span class="fit-content mx-auto text-h3">{{ 10 }}</span>
-          </div>
-        </v-col>
-      </v-card-text>
-    </v-card>
-    <v-card raised hover class="mb-6 no-cursor">
-      <v-card-title primary-title>
-        <v-icon color="primary" x-large left>mdi-currency-usd</v-icon>
-        Your Bounty</v-card-title
-      >
-      <v-card-text class="d-flex">
-        <v-col cols="4" class="d-flex align-start">
-          <div class="ml-8 cursor">
-            <h3 class="fit-content text-h3">$1,000</h3>
-          </div>
-        </v-col>
-      </v-card-text>
-    </v-card>
+    <v-skeleton-loader
+      v-if="$fetchState.pending"
+      type="card"
+    ></v-skeleton-loader>
+    <div v-else>
+      <v-card raised hover class="mb-6 no-cursor">
+        <v-card-title primary-title>
+          <v-icon color="primary" x-large left>mdi-shield-account</v-icon>
+          Programs and Submissions</v-card-title
+        >
+        <v-card-text class="d-flex">
+          <v-col cols="6" md="4" class="d-flex align-start">
+            <v-icon color="accent" left>mdi-shield-lock</v-icon>
+            <div class="cursor" @click="() => $router.push('/program')">
+              <h3 class="w-max">Private Programs</h3>
+              <span class="max-content mx-auto text-h3">{{
+                totalPrivateProgramsNumber
+              }}</span>
+            </div>
+          </v-col>
+          <v-col cols="6" md="4" class="d-flex align-start">
+            <v-icon color="accent" left>mdi-shield-link-variant</v-icon>
+            <div class="cursor" @click="() => $router.push('/submission')">
+              <h3 class="w-max">Your Submissions</h3>
+              <span class="fit-content mx-auto text-h3">{{
+                submissionsNumber
+              }}</span>
+            </div>
+          </v-col>
+        </v-card-text>
+      </v-card>
+      <v-card raised hover class="mb-6 no-cursor">
+        <v-card-title primary-title>
+          <v-icon color="primary" x-large left>mdi-podium-gold</v-icon>
+          Your Ranking</v-card-title
+        >
+        <v-card-text class="d-flex">
+          <v-col cols="6" md="4" class="d-flex align-start">
+            <v-icon color="accent" left>mdi-star-box</v-icon>
+            <div class="cursor">
+              <h3 class="w-max">Points Earned</h3>
+              <span class="fit-content mx-auto text-h3">{{ 10 }}</span>
+            </div>
+          </v-col>
+          <v-col cols="6" md="4" class="d-flex align-start">
+            <v-icon color="accent" left>mdi-seal</v-icon>
+            <div class="cursor">
+              <h3 class="w-max">Badges</h3>
+              <span class="fit-content mx-auto text-h3">{{ 10 }}</span>
+            </div>
+          </v-col>
+        </v-card-text>
+      </v-card>
+      <v-card raised hover class="mb-6 no-cursor">
+        <v-card-title primary-title>
+          <v-icon color="primary" x-large left>mdi-currency-usd</v-icon>
+          Your Bounty</v-card-title
+        >
+        <v-card-text class="d-flex">
+          <v-col cols="4" class="d-flex align-start">
+            <div class="ml-8 cursor">
+              <h3 class="fit-content text-h3">$1,000</h3>
+            </div>
+          </v-col>
+        </v-card-text>
+      </v-card>
+    </div>
   </v-main>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      totalPrivateProgramsNumber: null,
+      submissionsNumber: null,
+      badgesNumber: null,
+      bounty: null,
+    }
+  },
+  async fetch() {
+    await Promise.all([
+      this.getNumberOfPrivatePrograms(),
+      this.getNumberOfSubmissions(),
+    ])
+  },
   computed: {
     notificationState() {
       return this.$store.state.notification.showNotification
+    },
+  },
+  methods: {
+    async getNumberOfPrivatePrograms() {
+      const URL = `get-programs?page=1&limit=1000`
+      await this.$axios.$get(URL).then((res) => {
+        this.totalPrivateProgramsNumber = res.data.docs.filter(
+          (program) => program.private
+        ).length
+      })
+    },
+    async getNumberOfSubmissions() {
+      const URL = `get-all-submissions/all?page=1&limit=10`
+      await this.$axios.$get(URL).then(({ data: { docs = [] } }) => {
+        this.submissionsNumber = docs.length
+      })
     },
   },
 }
