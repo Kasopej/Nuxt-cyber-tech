6709df8999a7afe007cf0c17bb4bd9f47089b048
Kasope Johnson <kasopej@gmail.com>
2022-12-21 18:22:43 +0100

    customizes dark theme styles where needed

--- pages/program/index.vue
+++ pages/program/index.vue
@@ -20,11 +20,11 @@
     <v-col :cols="mobileView ? 12 : 10" class="px-4">
       <header class="d-flex mb-0 mb-md-6">
         <v-col cols="12">
-          <div class="mx-auto text-center">
+          <div class="mx-auto flex justify-center text-center">
             <input
               type="search"
               class="py-4 px-6 bg-primary text-white search-bar border-solid border rounded-lg border-primary"
-              style="width: 70%; margin: 0 15%"
+              style="width: 70%"
               placeholder="Search programs"
             />
             <v-icon class="relative right-12 cursor" color="white" x-large
