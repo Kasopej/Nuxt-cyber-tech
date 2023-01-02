86515e3b7186e37e490cfae39f52fdf5b1264d60
Kasope Johnson <kasopej@gmail.com>
2023-01-03 00:45:57 +0100

    adds ranking data to dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -33,11 +33,11 @@
       </v-card>
       <v-card raised hover class="mb-6 no-cursor">
         <v-card-title primary-title>
-          <v-icon color="primary" x-large left>mdi-podium-gold</v-icon>
-          Your Ranking</v-card-title
+          <v-icon color="primary" x-large left>mdi-podium</v-icon>
+          Your Stats</v-card-title
         >
         <v-card-text class="d-flex">
-          <v-col cols="6" md="4" class="d-flex align-start">
+          <v-col cols="4" class="d-flex align-start">
             <v-icon color="accent" left>mdi-star-box</v-icon>
             <div class="cursor">
               <h3 class="w-max">Points Earned</h3>
@@ -46,11 +46,20 @@
               }}</span>
             </div>
           </v-col>
-          <v-col cols="6" md="4" class="d-flex align-start">
+          <v-col cols="4" class="d-flex align-start">
             <v-icon color="accent" left>mdi-seal</v-icon>
             <div class="cursor">
               <h3 class="w-max">Badges</h3>
               <span class="fit-content mx-auto text-h3">{{ 10 }}</span>
+            </div>
+          </v-col>
+          <v-col cols="4" class="d-flex align-start">
+            <v-icon color="accent" left>mdi-podium-gold</v-icon>
+            <div class="cursor">
+              <h3 class="w-max">Ranking</h3>
+              <span class="fit-content mx-auto text-h3">{{
+                USER.user.ranking
+              }}</span>
             </div>
           </v-col>
         </v-card-text>
